78baa8d75ca16e49f5e92b4d81866373f3c509c4
unknown <aracelicatalano@gmail.com>
2023-05-10 21:43:20 -0300

    css fixes

--- nadyapokladweb/src/pages/Perfomances.jsx
+++ nadyapokladweb/src/pages/Perfomances.jsx
@@ -6,16 +6,17 @@ import useHookToRender from '../components/dashboard/FirebaseHooks/useHookToRend
 export function Perfomances() {
 
   const { performancesPost } = useHookToRender()
-  
 
   return (
     <>
-      <h4 className='title home-h5 mx-2'>Perfomances</h4><br />
+      <Container>
+        <h4 className='title home-h5 mx-2'>Perfomances</h4><br />
+      </Container>
       <Container>
         <Row>
           {
             performancesPost.map(item =>
-              <Col key={item.id} className='video-item'>
+              <Col key={item.id} className='video-item mb-5 '>
                 <VideoYouTube
                   url={item.url}
                   title={item.title} />
